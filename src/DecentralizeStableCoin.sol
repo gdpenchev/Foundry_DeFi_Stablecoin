2231a18376be968efa1e5634e15d8a3ea403e60a
gdpenchev <georgipenchev90@gmail.com>
2025-01-13 19:34:50 +0200

    Initial upload

---
+++ src/DecentralizeStableCoin.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity ^0.8.19;
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Georgi Penchev
+ * Collateral: Exogenous
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+* This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
+DSCEngine smart contract.
+ */
+
+contract DecentralizeStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    //this ERC20 is inherited from OpenZeppelin's ERC20Burnable contract
+    constructor()
+        ERC20("DecentralizedStableCoin", "DSC")
+        Ownable(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266)
+    {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (balance <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        //super keyword is used to call functions from the parent contract, this is the function frmo the ERC20Burnable contract
+        //here we are overriding the function and thats why we use super keyword
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
